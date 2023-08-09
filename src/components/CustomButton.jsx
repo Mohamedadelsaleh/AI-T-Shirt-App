193a46b5e3d4ed7daa5dbdee4e6e3b767030b7de
Mohamed Adel Saleh <mohamed.adel.saleh77@gmail.com>
2023-08-09 03:17:17 +0300

    Implement color picker and file picker

--- src/components/CustomButton.jsx
+++ src/components/CustomButton.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable react/prop-types */
 import { useSnapshot } from "valtio"
-import state from "../store"
+import state from "../store";
+import { getContrastingColor } from "../config/helpers";
 
 const CustomButton = ({type, title, handleClick, customStyles}) => {
 
@@ -10,7 +11,13 @@ const CustomButton = ({type, title, handleClick, customStyles}) => {
         if(type === 'filled') {
             return {
                 backgroundColor:snap.color,
-                color:'#fff',
+                color: getContrastingColor(snap.color)
+            }
+        }else if(type === 'outline'){
+            return {
+                borderWidth: '1px',
+                borderColor: snap.color,
+                color: snap.color,
             }
         }
     }
