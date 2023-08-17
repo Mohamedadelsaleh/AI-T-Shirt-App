df880bd73fce0b5bd27c34db10c8c1e0d5e17676
Mohamed Adel Saleh <mohamed.adel.saleh77@gmail.com>
2023-08-17 03:16:14 +0300

    add dalle routes

--- Back/index.js
+++ Back/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import * as dotenv from 'dotenv';
 import cors from 'cors';
+import dalleRoutes from './routes/dalle.routes.js';
 
 dotenv.config();
 
@@ -11,6 +12,8 @@ app.use(express.json({limit:"50mb"}));
 app.get('/', (req, res) => {
     res.status(200).json({ message: "OK"});
 })
+
+app.use('/api/v1/dalle', dalleRoutes)
 
 app.listen(8080, () => {
     console.log('Server listening on port 8080');
